b47526f5e6cbf124383ab245ced1a3efaa4d9997
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-22 22:23:24 +0000

    page fetching function and downloading a file using command prompt

---
+++ fetcher.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const fs = require('fs');
+
+const input = process.argv.slice(2);
+const URL = input[0];
+const localFilePath = input[1];
+let content;
+
+request(URL, (err, res, body) => {
+  if (err) {
+    console.log('Error: ', err);
+  }
+  // console.log('StatusCode', res && res.statusCode);
+  // console.log(body)
+
+  fs.writeFile(localFilePath, body, err => {
+    if (err) {
+      console.log(err)
+      return;
+    }
+    const stats = fs.statSync(localFilePath);
+    const fileSizeInBytes = stats.size;
+
+    console.log(`Downloaded ${fileSizeInBytes} bytes and saved to ${localFilePath}`);
+  })
+})
+
