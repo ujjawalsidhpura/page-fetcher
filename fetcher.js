8cc2dc50ee31f69dd1a713c2893d13c4694564e0
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-09-22 23:09:17 +0000

    interactivity added, func refactored

--- fetcher.js
+++ fetcher.js
@@ -1,27 +1,63 @@
 const request = require('request');
 const fs = require('fs');
 
+//Catching initial User Inputs
 const input = process.argv.slice(2);
 const URL = input[0];
 const localFilePath = input[1];
 
-request(URL, (err, res, body) => {
-  if (err) {
-    console.log('Error: ', err);
-  }
-  // console.log('StatusCode', res && res.statusCode);
-  // console.log(body)
-
-  fs.writeFile(localFilePath, body, err => {
-    if (err) {
-      console.log(err)
-      return;
-    }
-
-    const stats = fs.statSync(localFilePath);
-    const fileSizeInBytes = stats.size;
-
-    console.log(`Downloaded ${fileSizeInBytes} bytes and saved to ${localFilePath}`);
-  })
+//For further user interactivity
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 })
+
+//Main Function 
+const fileCreator = function (url) {
+
+  request(url, (err, res, body) => {
+    if (err) {
+      console.log('Error: ', err);
+    }
+    // console.log('StatusCode', res && res.statusCode);
+    // console.log(body)
+
+    fs.writeFile(localFilePath, body, err => {
+      if (err) {
+        console.log(err)
+        return;
+      }
+
+      const stats = fs.statSync(localFilePath);
+      const fileSizeInBytes = stats.size;
+
+      console.log(`Downloaded ${fileSizeInBytes} bytes and saved to ${localFilePath}`);
+    })
+  })
+
+  rl.close();
+}
+
+//Checking if FILE exists
+
+if (fs.existsSync(localFilePath)) {
+  rl.question('File already exist, do you want to overwrite? Press Y for Yes and N to terminate', (response) => {
+    if (response === 'y') {
+      fileCreator(URL);
+    } else if (response === 'n') {
+      rl.close();
+    }
+  })
+} else {
+  fileCreator(URL);
+}
+
+
+
+
+
+
+
+
 
